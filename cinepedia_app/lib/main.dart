9219743352b15325df9408b6a9f53f5f43116675
Anandan Mukherjee <anandanmukh@gmail.com>
2023-06-17 10:41:04 +0530

    cinepedia project

--- cinepedia_app/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:cinepedia_app/pages/splash_page.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(SplashPage(onInitializationComplete: () => Placeholder()));
-}
