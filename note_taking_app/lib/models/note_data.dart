e3bce885cb91816f9624ba1a2245587d736b2f3e
Anandan Mukherjee <anandanmukh@gmail.com>
2023-06-21 00:03:39 +0530

    Update note_data.dart

--- note_taking_app/lib/models/note_data.dart
+++ note_taking_app/lib/models/note_data.dart
@@ -1,22 +1,29 @@
+import 'package:flutter/material.dart';
+
 import 'note.dart';
 
-class NoteData {
+class NoteData extends ChangeNotifier {
   // overall list of notes
   List<Note> allNotes = [
     // default first note
     Note(
       id: 0,
-      text: 'Welcome to Note Taker!',
+      text: 'First Note',
+    ),
+    Note(
+      id: 1,
+      text: 'Second Note',
     ),
   ];
   // get notes
-  List<Note> getNotes() {
+  List<Note> getAllNotes() {
     return allNotes;
   }
 
   // add new note
   void addNewNote(Note note) {
     allNotes.add(note);
+    notifyListeners();
   }
 
   // update note
@@ -29,10 +36,12 @@ class NoteData {
         allNotes[i].text = text;
       }
     }
+    notifyListeners();
   }
 
   // delete note
   void deleteNode(Note note) {
     allNotes.remove(note);
+    notifyListeners();
   }
 }
