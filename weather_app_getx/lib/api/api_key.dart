6ea8ccf74d529288d377d491a57cb73eed32acc9
Anandan Mukherjee <anandanmukh@gmail.com>
2023-06-24 12:54:41 +0530

    removed dead code

--- weather_app_getx/lib/api/api_key.dart
+++
@@ -1,1 +0,0 @@
-const apiKey = "888dde2524704d1fcd4c4a0fe1c51f46";
--- weather_app_getx/lib/model/weather_data.dart
+++
@@ -1,7 +0,0 @@
-import 'package:weather_app_getx/model/weather_data_current.dart';
-
-class WeatherData {
-  final WeatherDataCurrent? current;
-
-  WeatherData([this.current]);
-}
